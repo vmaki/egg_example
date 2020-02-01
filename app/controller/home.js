f886296cc8ddf4197203f25c33d655ce1310724e
lindo <lindon_1@163.com>
2020-02-01 12:50:37 +0800

    加密测试

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,10 +2,24 @@
 
 const Controller = require('egg').Controller;
 
+const fs = require('fs');
+const crypto = require('crypto');
+
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
     ctx.body = 'hi, egg';
+  }
+
+  async crypto() {
+    const { ctx } = this;
+
+    const salt = crypto.randomBytes(128)
+      .toString('base64');
+    const hash = crypto.pbkdf2Sync('crypto', salt, 10000, 64, 'sha512')
+      .toString('hex');
+
+    ctx.body = hash;
   }
 }
 
